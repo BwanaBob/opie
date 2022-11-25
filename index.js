48f2ec180edda579b4324f8c7adca288f38694f9
BwanaBob <bowanabob@yahoo.com>
2022-11-25 17:52:02 -0600

    Identify GIFs

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+const { GatewayIntentBits } = require("discord.js")
 const Discord = require("discord.js")
 require("dotenv").config()
 const client = new Discord.Client({
     intents: [
-        "Guilds",
-        "GuildMessages",
-        "MessageContent"
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
     ]
 })
+
+const gifexpr = new RegExp("(http|https|ftp):\/\/.*(.gif|.png)")
+
+const gifusers = {}
 
 client.on("ready", () => {
     console.log(`Logged in as: ${client.user.tag}`)
@@ -14,9 +19,16 @@ client.on("ready", () => {
 
 client.on("messageCreate", (message) => {
 //    console.log(`Message: ${message.content}`)
-//    console.log(`Message: ${message.id}`)
+
+    if(gifexpr.test(message.content)){
+        message.react('🔥')
+    }
+
     if(message.content == "opie"){
         message.reply("I am not a robot")
+        console.log(message.author.id)
+        console.log(message.author.username)
+        console.log(message.author.discriminator)
     }
 })
 
