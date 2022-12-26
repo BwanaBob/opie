91a01ceb78123d4b6a47770d08964066d92f78c7
BwanaBob <bowanabob@yahoo.com>
2022-12-26 08:57:12 -0600

    Added emojis

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -31,12 +31,14 @@ module.exports = {
         "🥂",
         "🎆",
         "🎇",
+        "🎊",
         `<a:partying:1040138276692033556>`,
         `<a:party_blob:1038194657848463370>`,
         `<a:party_popper:1038207072485003274>`,
         `<a:bottle_with_popping_cork:1056931540283699282>`,
         `<a:firework:1010870326667788298>`,
         `<a:cat_jump:1040302608813924415>`,
+        `<a:confetti_ball_animated:1056947556241903636>`,
       ];
       const reaction = reactions[Math.floor(Math.random() * reactions.length)];
       message.react(reaction);
