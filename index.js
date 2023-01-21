d4655998c1ae606e105f2776c3f0dfcfd23c94f4
BwanaBob <bowanabob@yahoo.com>
2023-01-21 13:07:25 -0600

    Cosmetic

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-	GatewayIntentBits.GuildMembers
+	  GatewayIntentBits.GuildMembers
   ],
 });
 
