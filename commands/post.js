831b1ccfe851834ff391e4b5467b1c02f4d2ce62
BwanaRob <bwanabob@gmail.com>
2023-03-30 23:07:26 -0500

    showtime

--- commands/post.js
+++ commands/post.js
@@ -25,6 +25,27 @@ const bingoEmbed = new EmbedBuilder()
     "https://i.imgur.com/dJP9d8L.png"
 );
 
+const showtimeEmbed = new EmbedBuilder()
+.setColor(0x0000ff)
+.setTitle("Showtime")
+.setDescription("Welcome and enjoy the show!\nPlease read the rules before posting.")
+.setThumbnail("https://i.imgur.com/fJ12AKT.png")
+.setImage('https://i.imgur.com/1oZPjOW.png')
+.addFields({
+  name: "Rules",
+  value: `[#rules](https://discord.com/channels/325206992413130753/1000869946215120987)`,
+  inline: true})
+.addFields({
+  name: "Bingo",
+  value: `[thatsabingo.com](https://www.thatsabingo.com/)`,
+  inline: true})
+.addFields({
+  name: "Reddit",
+  value: `[r/OnPatrolLive](https://www.reddit.com/r/OnPatrolLive/)`,
+  inline: true})
+//.setFooter({ text: "Showtime: \<t:1680307200:f> - <t:1680307200:R>" });
+;
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("post")
@@ -36,8 +57,9 @@ module.exports = {
         .setDescription('The message to post')
         .setRequired(true)
         .addChoices(
-          { name: 'kids', value: 'post_kids' },
-          { name: 'bingo', value: 'post_bingo' },
+          { name: 'kids',     value: 'post_kids'     },
+          { name: 'bingo',    value: 'post_bingo'    },
+          { name: 'showtime', value: 'post_showtime' },
         ))
       // .addChannelOption(option =>
       //   option.setName('channel')
@@ -54,6 +76,10 @@ module.exports = {
         break;
         case "post_bingo" : {
           postEmbed = bingoEmbed;
+        }
+        break;
+        case "post_showtime" : {
+          postEmbed = showtimeEmbed;
         }
         break;
       }
