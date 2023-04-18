1e4f31a0cf8feea5a9369a510fb1feab8f35066a
BwanaRob <bwanabob@gmail.com>
2023-04-18 15:58:34 -0500

    OpenAI fix history and names

--- modules/openai.js
+++ modules/openai.js
@@ -7,54 +7,52 @@ const openai = new OpenAIApi(configuration);
 
 module.exports = async function (message) {
   // try {
-    await message.channel.sendTyping();
-    let conversationLog = [{ role: 'system', content: 'Respond like a friendly, snarky, chatbot kitten named OPie' }];
-    let prevMessages = await message.channel.messages.fetch({ limit: 15 });
-    let userCount = 0;
-    let botCount = 0;
-    prevMessages.forEach((msg) => {
-      //console.log(msg.content);
-      switch (msg.author.id) {
-        case message.client.user.id:
-          if (botCount < 3) {
-            botCount++;
-            conversationLog.push({
-              role: 'assistant',
-              content: msg.content,
-              name: msg.member.displayName,
-            });
-            // console.log(`User : ${msg.memberdisplayName}`);
-            break;
-          }
-        case message.author.id:
-          if (userCount < 3 && message.content.match(/^(opie,\W)/gi)) {
-            userCount++;
-            conversationLog.push({
-              role: 'user',
-              content: msg.content.substr(6),
-              name: msg.member.displayName,
-            });
-            // console.log(`User : ${msg.member.displayName}`);
-            //console.log(`User: ${msg.content.substr(6)}`);
-            break;
-          }
-      }
-    });
-    conversationLog.reverse();
-
-    const result = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: conversationLog,
-      max_tokens: 192, // limit token usage (length of response)
-    })
-      .catch((error) => {
-        console.log(`OPENAI ERR: ${error}`);
+  await message.channel.sendTyping();
+  let conversationLog = new Array
+  let prevMessages = await message.channel.messages.fetch({ limit: 15 });
+  let userCount = 0;
+  let botCount = 0;
+  prevMessages.forEach((msg) => {
+    //console.log(msg.content);
+    // switch (msg.author.id) {
+    //   case message.client.user.id:
+    if (msg.author.id == message.client.user.id && botCount < 3) {
+      botCount++;
+      conversationLog.unshift({
+        role: 'assistant',
+        content: msg.content,
+        //name: msg.member.displayName,
       });
-  // } catch (error) {
-  //   console.log(`ERROR: ${error}`);
-  // }
+      //console.log(`Bot  : ${msg.member.displayName} Msg: ${msg.content}`);
+    } else if (msg.author.id == message.author.id
+      && userCount < 3
+      && msg.content.match(/^(opie,\W)/gi)) {
+      userCount++;
+      conversationLog.unshift({
+        role: 'user',
+        content: msg.content.substr(6),
+        //name: msg.member.displayName,
+      });
+      //console.log(`User : ${msg.member.displayName} Msg: ${msg.content.substr(6)}`);
+    }
+  })
 
-  if (typeof result !== 'undefined') {
-    return result.data.choices[0].message.content
-  } else { return "ERR" }
+//conversationLog.reverse();
+conversationLog.unshift ({ role: 'system', content: 'Respond like a friendly, snarky, chatbot kitten named OPie' });
+
+const result = await openai.createChatCompletion({
+  model: 'gpt-3.5-turbo',
+  messages: conversationLog,
+  max_tokens: 192, // limit token usage (length of response)
+})
+  .catch((error) => {
+    console.log(`OPENAI ERR: ${error}`);
+  });
+// } catch (error) {
+//   console.log(`ERROR: ${error}`);
+// }
+
+if (typeof result !== 'undefined') {
+  return result.data.choices[0].message.content
+} else { return "ERR" }
 }
