b867f4017c473b2a54e6a57c81f215836abebfa1
BwanaRob <bwanabob@gmail.com>
2023-05-27 12:49:40 -0500

    Nuggies

--- reactions/dinoNuggets.js
+++ reactions/dinoNuggets.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "DinoNuggets",
     logName: "🐢 D NUGS",
-    regex: "\\bdino(saur)?(s)?\\b.{1,25}\\bnug(get)?(s)?\\b",
+    regex: "\\bdino(saur)?(s)?\\b.{1,25}\\bnug(g)?(et|ie)?(s)?\\b",
     async execute(message) {
         if (message.guild.id == "325206992413130753") {
             message.react(`<:dino_nugget:1111766293897560197>`);
--- reactions/dinosaur.js
+++ reactions/dinosaur.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "Dinosaur",
     logName: "🐢 DINO  ",
-    regex: "\\bdino(saur)?(s)?\\b(?!.{1,55}\\bnug(get)?(s)?\\b)",
+    regex: "\\bdino(saur)?(s)?\\b(?!.{1,55}\\bnug(g)?(et|ie)?(s)?\\b)",
     async execute(message) {
         message.react('🦕')
     }
