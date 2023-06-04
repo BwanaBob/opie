d155a2fc587db288d9673859f50f051cd9a8a9f9
BwanaRob <bwanabob@gmail.com>
2023-06-03 23:59:11 -0500

    Lets Go Regex Fix

--- reactions/letsGo.js
+++ reactions/letsGo.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "LetsGo",
     logName: "🚨 LETSGO",
-    regex: "(here we|lets|let's) go",
+    regex: "\\b(here we|lets|let's) go\\b",
     async execute(message) {
         if (message.guild.id == "325206992413130753") {
             message.react(`<a:police_car_light:1038193703854030878>`);
