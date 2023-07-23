124c79275ded7869492dba1398a433dcf6a84960
BwanaRob <bwanabob@gmail.com>
2023-07-23 15:45:10 -0500

    Here We Go Fix

--- reactions/letsGo.js
+++ reactions/letsGo.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "LetsGo",
     logName: "🚨 LETSGO",
-    regex: "\\b(here we|lets|let's) go\\b",
+    regex: "\\b(here(.{0,12})we|let(.)?s)(.{0,12})go(o)*\\b",
     async execute(message) {
         if (message.guild.id == "325206992413130753") {
             message.react(`<a:police_car_light:1038193703854030878>`)
