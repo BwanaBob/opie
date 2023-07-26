4ca8daccd833cb7f9cceab64192fdc86f2b66592
BwanaRob <bwanabob@gmail.com>
2023-07-26 11:53:08 -0500

    LAFR Reminder update

--- jobs/laFireThread.js
+++ jobs/laFireThread.js
@@ -3,19 +3,23 @@ const CronJob = require('cron').CronJob;
 module.exports = {
     execute(client) {
         const noticeChannel = client.channels.cache.get("1119423219431116940") || client.channels.cache.get("1045327770592497694"); // LAFR or OPie #notifications
-
+        const noticeRole = '1119424005175246992'; // LAFR Moderator
         const jobLoadedDate = new Date().toLocaleString();
         console.log(`[${jobLoadedDate}] ⌛ CRON  | Job Loaded    | LAFR Thread Reminder`);
         var jobStartRecording = new CronJob(
             '00 00 18 * * TUE', () => {
                 // '*/15 * * * * *', () => {
                 if (client.params.get("chatGPTAnnouncementsEnabled") == 'true') {
-                    try {noticeChannel.send("<@&1119424005175246992> Update the thread for tomorrow's show!");}
-                    catch (err) { console.error(`[ERROR] Sending message -`, err.message); }
-                    // try { client.users.send('348629137080057878', "Start Recording"); } // PM Bwana
-                    // catch (err) { console.error(`[ERROR] Sending message -`, err.message); }
+                    const showEndDate = new Date('2023-08-10');
+                    const nowDate = new Date();
                     const jobExecutedDate = new Date().toLocaleString();
-                    console.log(`[${jobExecutedDate}] ⌛ CRON  | Job Executed  | LAFR Thread Reminder`);
+                    if (nowDate > showEndDate) {
+                        console.log(`[${jobExecutedDate}] ⌛ CRON  | Job Executed  | LAFR Thread Reminder (Show Ended)`);
+                    } else {
+                        try { noticeChannel.send(`<@&${noticeRole}>,\nUpdate the scheduled Reddit [live thread](https://www.reddit.com/r/LAFireandRescue/about/scheduledposts) for tomorrow's __**LA Fire & Rescue**__ episode!\nGet your information [here](https://tvlistings.zap2it.com/overview.html?tabName=guide&programSeriesId=SH04716491&aid=gapzap)`); }
+                        catch (err) { console.error(`[ERROR] Sending message -`, err.message); }
+                        console.log(`[${jobExecutedDate}] ⌛ CRON  | Job Executed  | LAFR Thread Reminder`);
+                    }
                 }
             },
             null,
