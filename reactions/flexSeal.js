58f56946d511617111650ba2707ed63203b5676b
BwanaRob <bwanabob@gmail.com>
2023-07-30 02:07:47 -0500

    Flex Seal

---
+++ reactions/flexSeal.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: "FlexSeal",
+    logName: "💪 F-SEAL",
+    regex: "flex.seal",
+    async execute(message) {
+        if (message.guild.id == "325206992413130753") {
+            message.react(`<:flex_seal:1135098661941022740>`)
+                .catch(err => { console.error(`[ERROR] Reacting to message ${message.id} -`, err.message); });
+        } else {
+            message.react(`💪`)
+                .catch(err => { console.error(`[ERROR] Reacting to message ${message.id} -`, err.message); });
+        }
+    }
+}
