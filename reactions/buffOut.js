916b206f9593693a64defaf846747722bf2ca342
BwanaRob <bwanabob@gmail.com>
2023-08-04 14:37:46 -0500

    buff out image

--- reactions/buffOut.js
+++ reactions/buffOut.js
@@ -5,9 +5,9 @@ module.exports = {
     logName: "🚗 BUFOUT",
     regex: "(Buff Out)",
     async execute(message) {
-        if (message.author.tag == "Sausageslinger11#3264") {
+        if ((message.author.id == "440328038337478657") || (message.author.id == "687155636244578304")) { //saucy or bwana's alt
             // message.channel.send(`🚗 Come on down to ${message.member}'s car repair shop! Everything can be buffed out! 🚗`);
-            const image = new AttachmentBuilder('https://i.imgur.com/gF0aNsT.png');
+            const image = new AttachmentBuilder('./resources/reaction-buff-out.png');
             message.reply({ files: [image] })
         } else {
             message.react('🚗')
