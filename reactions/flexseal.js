9fe931286e738d9510f8c717d453e4188a24b31d
BwanaRob <bwanabob@gmail.com>
2023-08-16 03:54:45 -0500

    Flex Seal ReactImage

---
+++ reactions/flexseal.js
@@ -0,0 +1,26 @@
+const { AttachmentBuilder } = require("discord.js");
+
+module.exports = {
+    name: "FlexSeal",
+    logName: "💪 F-SEAL",
+    regex: "\\bflex\\Wseal\\b",
+    async execute(message) {
+        const imageDelay = 900;
+        const lastImage = message.client.timers.get("image-flex-seal") ?? imageDelay + 100;
+        const elapsed = Math.trunc((message.createdTimestamp - lastImage) / 1000);
+        if (elapsed > imageDelay) {
+            message.client.timers.set("image-flex-seal", message.createdTimestamp);
+            const replyImage = new AttachmentBuilder("./resources/reaction-flex-seal.png", { name: "reaction-flex-seal.png" });
+            message.reply({ files: [replyImage] })
+                .catch(err => { console.error(`[ERROR] Relpying to message ${message.id} -`, err.message); });
+        } else {
+            if (message.guild.id == "325206992413130753") {
+                message.react(`<:flex_seal:1135098661941022740>`)
+                    .catch(err => { console.error(`[ERROR] Reacting to message ${message.id} -`, err.message); });
+            } else {
+                message.react(`💪`)
+                    .catch(err => { console.error(`[ERROR] Reacting to message ${message.id} -`, err.message); });
+            }
+        }
+    }
+}
