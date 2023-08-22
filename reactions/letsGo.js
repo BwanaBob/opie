149dcfef9ee468557af361f7c8d1f23aa09bf014
BwanaRob <bwanabob@gmail.com>
2023-08-22 11:44:55 -0500

    Guild Emoji Test

--- reactions/letsGo.js
+++ reactions/letsGo.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "LetsGo",
     logName: "🚨 LETSGO",
-    regex: "\\b((here\\b((.){0,12})\\bwe|let(.)?s)\\b((.){0,12})\\bgo(o)*)\\b|\\b(va(a)*mo(o)*s(s)*)\\b",
+    regex: "\\b((here\\b((.){0,12})\\bwe|let(.)?s)\\b((.){0,12})\\bgo(o)*)\\b|\\b(va(a)*m(a(a)*n)?o(o)*s(s)*)\\b",
     async execute(message) {
-        if (message.guild.id == "325206992413130753") {
+        if (message.client.guilds.cache.get('325206992413130753')) {
             message.react(`<a:police_car_light:1038193703854030878>`)
                 .catch(err => { console.error(`[ERROR] Reacting to message ${message.id} -`, err.message); });
         } else {
