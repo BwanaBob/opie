e3199412c258194b71a5e780227ec03c7315cf49
BwanaRob <bwanabob@gmail.com>
2023-12-09 14:33:37 -0600

    Reminder Links

--- jobs/reminderTodoFri.js
+++ jobs/reminderTodoFri.js
@@ -16,7 +16,7 @@ module.exports = {
                         noticeRole = '325210261722234881';  // OPL Admin
                         reacts = ["🧵", "<:bingo:1066838689814163466>", "🤖", "📋", "💬", "⏺️", "🧹", "🏅"];
                     }
-                    let noticeContent = `<@&${noticeRole}>\n## Friday Checklist\n### Pre-show\n- 🧵 [Thread](<https://www.reddit.com/r/OnPatrolLive/about/scheduledposts>) scheduled at <t:${scheduleTime}:t> <t:${scheduleTime}:R>\n- ${reacts[1]} [Bingo](<https://www.thatsabingo.com/>) Reset\n- 🤖 OPie prompts\n- 📋 Lineup\n- 💬 Opening comment\n- ⏺️ First Shift Recording\n### After\n- 🧹 Closing and !tidy\n- 🏅 Comment of the Night Awards`;
+                    let noticeContent = `<@&${noticeRole}>\n## Friday Checklist\n### Pre-show\n- 🧵 [Thread](<https://www.reddit.com/r/OnPatrolLive/about/scheduledposts>) scheduled at <t:${scheduleTime}:t> <t:${scheduleTime}:R>\n- ${reacts[1]} [Bingo](<https://www.thatsabingo.com/>) Reset\n- 🤖 [OPie prompts](<https://github.com/BwanaBob/opie/commits/main/>)\n- 📋 [Lineup](<https://twitter.com/danabrams>)\n- 💬 Opening comment\n- ⏺️ [First Shift Recording](<https://www.reelznow.com/live>)\n### After\n- 🧹 Closing and !tidy\n- 🏅 Comment of the Night Awards`;
                     const noticeMessage = await noticeChannel.send({ content: noticeContent })
                         .catch(err => { console.error(`[ERROR] Sending message: `, err.message); });
                     noticeMessage.react(reacts[0])
