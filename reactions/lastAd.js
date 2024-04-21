ce9bbd7213888e5c90dd2d04196aa3d807c41dbf
BwanaRob <bwanabob@gmail.com>
2024-04-20 23:35:48 -0500

    LastAd time window fix

--- reactions/lastAd.js
+++ reactions/lastAd.js
@@ -3,19 +3,20 @@ const { AttachmentBuilder } = require("discord.js");
 module.exports = {
     name: "LastAd",
     logName: "📺 LASTAD",
-    regex: "(?:4|7).minute(?:s)?",
+    regex: "(?:4|7|four|seven).minute(?:s)?",
     async execute(message) {
         const imageDelay = 5400; // seconds 5400 = 1.5hr
         const lastImage = message.client.timers.get("image-last-ad") ?? imageDelay + 100;
         const elapsed = Math.trunc((message.createdTimestamp - lastImage) / 1000);
         const now = new Date();
         const currentDay = now.getUTCDay();
-        const currentHour = now.getUTCHours() - 5; // UTC to Central Time conversion
+        const currentHour = now.getUTCHours();
         const currentMinute = now.getUTCMinutes();
+        console.log(`D: ${currentDay} H:${currentHour} M:${currentMinute} Id:${message.author.id}`);
         if ((message.author.id == '844616406293151745' &&
             (elapsed > imageDelay) &&
-            (currentDay === 5 || currentDay === 6) &&
-            currentHour === 22 &&
+            (currentDay === 6 || currentDay === 0) &&
+            currentHour === 3 &&
             currentMinute >= 46) ||
             (message.author.id == '348629137080057878')) {
             message.client.timers.set("image-last-ad", message.createdTimestamp);
