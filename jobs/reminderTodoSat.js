eedc08c85e223a58641abc730379ae1db9f9cc68
BwanaRob <bwanabob@gmail.com>
2024-05-11 12:37:45 -0500

    Community Status Reminder

--- jobs/reminderTodoSat.js
+++ jobs/reminderTodoSat.js
@@ -9,14 +9,14 @@ module.exports = {
             '00 00 12 * * SAT', async () => {
             // '*/25 * * * * *', async () => {
                 let noticeRole = '391837678967980035'; // OPie Test Role
-                let reacts = ["🧵", "🎱", "🤖", "📋", "💬", "⏺️", "🧹", "🏅", "📊"];
+                let reacts = ["🧵", "🎱", "🤖", "📋", "💬", "⏺️", "🚨", "🧹", "🏅", "📊"];
                 const scheduleTime = Math.floor(new Date().setHours(18, 0, 0) / 1000);
                 if (client.params.get("chatGPTAnnouncementsEnabled") == 'true') {
                     if (client.guilds.cache.get('325206992413130753')) { //bot is a member of OPL
                         noticeRole = '325210261722234881';  // OPL Admin
-                        reacts = ["🧵", "<:bingo:1066838689814163466>", "🤖", "📋", "💬", "⏺️", "🧹", "🏅", "📊"];
+                        reacts = ["🧵", "<:bingo:1066838689814163466>", "🤖", "📋", "💬", "⏺️", "🚨", "🧹", "🏅", "📊"];
                     }
-                    let noticeContent = `<@&${noticeRole}>\n## Saturday Checklist\n### Pre-show\n🧵 [Thread](<https://www.reddit.com/r/OnPatrolLive/about/scheduledposts>) scheduled at <t:${scheduleTime}:t> <t:${scheduleTime}:R>\n${reacts[1]} [Bingo](<https://www.thatsabingo.com/>) Reset\n🤖 [OPie prompts](<https://github.com/BwanaBob/opie/commits/main/>)\n📋 [Lineup](<https://twitter.com/danabrams>)\n💬 Opening comment\n⏺️ [First Shift Recording](<https://www.reelznow.com/live>)\n### After\n🧹 Closing and !tidy\n🏅 Comment of the Night Awards\n📊 Compile Stats`;
+                    let noticeContent = `<@&${noticeRole}>\n## Saturday Checklist\n### Pre-show\n🧵 [Thread](<https://www.reddit.com/r/OnPatrolLive/about/wiki/moderation/live-thread/>) scheduled at <t:${scheduleTime}:t> <t:${scheduleTime}:R>\n${reacts[1]} [Bingo](<https://www.thatsabingo.com/>) Reset\n🤖 [OPie prompts](<https://github.com/BwanaBob/opie/commits/main/>)\n📋 [Lineup](<https://twitter.com/danabrams>)\n💬 Opening comment\n⏺️ [First Shift Recording](<https://www.reelznow.com/live>)\n🚨 Community Status\n### After\n🧹 Closing and !tidy\n🏅 Comment of the Night Awards\n📊 Compile Stats`;
                     const noticeMessage = await noticeChannel.send({ content: noticeContent })
                         .catch(err => { console.error(`[ERROR] Sending message: `, err.message); });
                     noticeMessage.react(reacts[0])
@@ -28,6 +28,7 @@ module.exports = {
                         .then(() => noticeMessage.react(reacts[6]))
                         .then(() => noticeMessage.react(reacts[7]))
                         .then(() => noticeMessage.react(reacts[8]))
+                        .then(() => noticeMessage.react(reacts[9]))
                         .catch(err => { console.error(`[ERROR] Reacting to message ${noticeMessage.id} -`, err.message); });
                     const jobExecutedDate = new Date().toLocaleString();
                     console.log(`[${jobExecutedDate}] ⌛ CRON  | Job Executed  | Reminder Todo Sat`);
