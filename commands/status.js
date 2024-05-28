b249a830a47479fd9e74032ca1214eee11dccd95
BwanaRob <bwanabob@gmail.com>
2024-05-27 22:33:26 -0500

    Custom Status

--- commands/status.js
+++ commands/status.js
@@ -9,67 +9,72 @@ module.exports = {
     .setDMPermission(false)
     .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
     .addStringOption(option =>
-        option.setName('activity')
+      option.setName('activity')
         .setDescription('Activity Type')
         .setRequired(true)
         .addChoices(
-            { name: 'Watching',  value: 'status_watching'  },
-            { name: 'Playing',   value: 'status_playing'   },
-            { name: 'Listening', value: 'status_listening' },
-//            { name: 'Streaming', value: 'status_streaming' },
-            { name: 'Competing', value: 'status_competing' },
-//            { name: 'Custom',    value: 'status_custom'    },
-          )  
+          { name: 'Watching', value: 'status_watching' },
+          { name: 'Playing', value: 'status_playing' },
+          { name: 'Listening', value: 'status_listening' },
+          //            { name: 'Streaming', value: 'status_streaming' },
+          { name: 'Competing', value: 'status_competing' },
+          { name: 'Custom', value: 'status_custom' },
         )
-        .addStringOption(option =>
-            option.setName('text')
-            .setDescription('Status text')
-            .setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('text')
+        .setDescription('Status text')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('status')
+        .setDescription('Online Status')
+        .setRequired(false)
+        .addChoices(
+          { name: 'Online', value: 'online' },
+          { name: 'Idle', value: 'idle' },
+          { name: 'Do not disturb', value: 'dnd' },
+          { name: 'Invisible', value: 'invisible' },
         )
-        .addStringOption(option =>
-            option.setName('status')
-            .setDescription('Online Status')
-            .setRequired(false)
-            .addChoices(
-                { name: 'Online',         value: 'online'    },
-                { name: 'Idle',           value: 'idle'      },
-                { name: 'Do not disturb', value: 'dnd'       },
-                { name: 'Invisible',      value: 'invisible' },
-            )  
-        ),
-        async execute(interaction) {
-        const presenceType = interaction.options.getString('activity') ?? 'status_watching';
-        const presenceText = interaction.options.getString('text') ?? 'Nothing';
-        const presenceStatus = interaction.options.getString('status') ?? 'online';
-        var parsedType = ActivityType.Watching;
+    ),
+  async execute(interaction) {
+    const presenceType = interaction.options.getString('activity') ?? 'status_watching';
+    const presenceText = interaction.options.getString('text') ?? 'Nothing';
+    const presenceStatus = interaction.options.getString('status') ?? 'online';
+    var parsedType = ActivityType.Watching;
 
-        switch (presenceType) {
-            case 'status_watching':
-                parsedType = ActivityType.Watching;
-                break;
-            case 'status_playing':
-                parsedType = ActivityType.Playing;
-                break;
-            case 'status_listening':
-                parsedType = ActivityType.Listening;
-              break;
-            case 'status_streaming':
-                parsedType = ActivityType.Streaming;
-              break;
-            case 'status_competing':
-                parsedType = ActivityType.Competing;
-          }
+    switch (presenceType) {
+      case 'status_watching':
+        parsedType = ActivityType.Watching;
+        break;
+      case 'status_playing':
+        parsedType = ActivityType.Playing;
+        break;
+      case 'status_listening':
+        parsedType = ActivityType.Listening;
+        break;
+      case 'status_streaming':
+        parsedType = ActivityType.Streaming;
+        break;
+      case 'status_custom':
+        parsedType = ActivityType.Custom;
+        break;
+      case 'status_competing':
+        parsedType = ActivityType.Competing;
+    }
 
-        let newStatus = {
-            status: presenceStatus,
-            activities: [
-              {
-                name: presenceText,
-                type: parsedType,
-              },
-            ],
-        }
-        interaction.client.user.setPresence(newStatus);
-        await interaction.reply({ content: statusReply, ephemeral: true });
-    },
+    let newStatus = {
+      status: presenceStatus,
+      activities: [{ type: parsedType, name: presenceText, },],
+    }
+    if (presenceType === "status_custom") {
+      newStatus = {
+        status: presenceStatus,
+        activities: [{ type: parsedType, name: presenceText, state: presenceText, },],
+      }
+    }
+
+    interaction.client.user.setPresence(newStatus);
+    await interaction.reply({ content: statusReply, ephemeral: true });
+  },
 };
