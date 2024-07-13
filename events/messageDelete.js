d0b46aa147ae48a7ab4e04425312928a269b9b49
BwanaBob <bowanabob@yahoo.com>
2024-07-12 21:01:46 -0500

    Delete bugfix

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,28 +1,41 @@
-const { Events, EmbedBuilder, PermissionsBitField, AttachmentBuilder } = require("discord.js");
+const {
+  Events,
+  EmbedBuilder,
+  PermissionsBitField,
+  AttachmentBuilder,
+} = require("discord.js");
 const options = require("../options.json");
 
 module.exports = {
   name: Events.MessageDelete,
   async execute(message) {
     const logDate = new Date(message.createdTimestamp).toLocaleString();
-    const memberDisplayName = message.member?.displayName ?? "unknown";
-    const memberDisplayAvatarURL = message.member?.displayAvatarURL ?? null;
+    let memberDisplayName = "unknown";
+    let memberDisplayAvatarURL = null;
+    if (message.member) {
+      memberDisplayAvatarURL = message.member.memberDisplayAvatarURL;
+      memberDisplayName = message.member.displayName;
+    }
     console.log(
       `[${logDate}] 🚮 DELETE| ${message.guild.name} | ${message.channel.name} | ${memberDisplayName} (${message.author.tag}) | Deleted`
     );
 
     if (
       message.author.bot ||
-      ( message.member && message.member.permissions.has(
-        PermissionsBitField.Flags.ManageMessages
-      )) ||
+      (message.member &&
+        message.member.permissions.has(
+          PermissionsBitField.Flags.ManageMessages
+        )) ||
       message.channel.name == "notifications" ||
       message.channel.name == "art" // midjourney message
     ) {
       return;
     }
 
-    const noticeImage = new AttachmentBuilder("./resources/thumb-wastebasket.png", {name: "thumb-wastebasket.png"});
+    const noticeImage = new AttachmentBuilder(
+      "./resources/thumb-wastebasket.png",
+      { name: "thumb-wastebasket.png" }
+    );
     const noticeEmbed = new EmbedBuilder()
       .setColor(options.embeds.messageDeleted.color)
       .setTitle(options.embeds.messageDeleted.title)
@@ -41,8 +54,9 @@ module.exports = {
     }
     if (message.attachments.size !== 0) {
       const removedAttachmentURL = message.attachments.first()?.url || " ";
-      const removedAttachmentType = message.attachments.first()?.contentType || " ";
-      if (removedAttachmentType.includes('image/')) {
+      const removedAttachmentType =
+        message.attachments.first()?.contentType || " ";
+      if (removedAttachmentType.includes("image/")) {
         noticeEmbed.setImage(removedAttachmentURL);
       } else {
         noticeEmbed.addFields({
@@ -57,10 +71,10 @@ module.exports = {
       if (embedProvider == "Tenor") {
         var tenorThumbnail = message.embeds[0]?.thumbnail?.url ?? "";
         var tenorURL = message.embeds[0]?.url ?? "";
-        const tenorGifURL = await fetch(`${tenorURL}.gif`)
-          .then((response) => {
+        const tenorGifURL =
+          (await fetch(`${tenorURL}.gif`).then((response) => {
             return response.url;
-          }) ?? "";
+          })) ?? "";
         if (tenorGifURL) {
           noticeEmbed.setImage(tenorGifURL);
         } else {
