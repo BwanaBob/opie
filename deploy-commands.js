e50e03d623d32c5233b98f2bbbf42524a28e1db0
BwanaBob <bowanabob@yahoo.com>
2025-08-20 12:11:56 -0500

    ✨ sideline the mystats command until after the first show

--- deploy-commands.js
+++ deploy-commands.js
@@ -16,6 +16,7 @@ const commandFiles = fs
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
+  console.log(`Loaded command: ${command.data.name}`);
   commands.push(command.data.toJSON());
 }
 
